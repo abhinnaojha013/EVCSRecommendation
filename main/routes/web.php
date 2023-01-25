320ffbe885d0090d65f2d5e78e3d184af0816d0e
Abhinna Ojha <abhinnacsit18@gmail.com>
2023-01-25 11:39:27 +0545

    Update routes

--- main/routes/web.php
+++ main/routes/web.php
@@ -20,3 +20,13 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+
+Route::get('/Charging-Station/create', [\App\Http\Controllers\ChargingStationController::class, 'create'])
+    ->name('chargingStation.create');
+Route::post('/Charging-Station/store', [\App\Http\Controllers\ChargingStationController::class, 'store'])
+    ->name('chargingStation.store');
+Route::post('/Charging-Station/getDistricts', [\App\Http\Controllers\ChargingStationController::class, 'getDistricts']);
+Route::post('/Charging-Station/getMetropolitans', [\App\Http\Controllers\ChargingStationController::class, 'getMetropolitans']);
+Route::post('/Charging-Station/getWards', [\App\Http\Controllers\ChargingStationController::class, 'getWards']);
