b6118b62b9c333c0f08a5b0161d5381394c604e3
Abhinna Ojha <abhinnacsit18@gmail.com>
2023-03-11 19:16:32 +0545

    Update models

--- main/app/Models/Ratings.php
+++ main/app/Models/Ratings.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Carbon\Carbon;
+use http\Env\Request;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Auth;
@@ -11,6 +12,31 @@ use Illuminate\Support\Facades\DB;
 class Ratings extends Model
 {
     use HasFactory;
+
+    function oldUserRating($request) {
+        return DB::table('ratings')
+            ->where('user', '=', Auth::id())
+            ->where('charging_station', '=', $request->charging_station)
+            ->get();
+    }
+
+    function getUserChargingStationRating($id) {
+        return DB::table('ratings')
+            ->join('charging_stations', 'ratings.charging_station', '=', 'charging_stations.id')
+            ->join('locations','charging_stations.location','=', 'locations.id')
+            ->join('metropolitans', 'locations.metropolitan', '=', 'metropolitans.id')
+            ->join('districts', 'metropolitans.district', '=', 'districts.id')
+            ->join('provinces', 'districts.province', '=', 'provinces.id')
+            ->select('ratings.id as r_id',
+                'charging_stations.charging_station_name as cs_name',
+                'ratings.rating as rating',
+                'locations.ward_number as ward_number',
+                'metropolitans.metropolitan_name as metropolitan',
+                'districts.district_name as district',
+                'provinces.province_name as province'
+            )->where('ratings.user', '=', $id)
+            ->get();
+    }
 
     function insertRating($request) {
         $now = Carbon::now();
@@ -22,5 +48,35 @@ class Ratings extends Model
             'created_at' => $now,
             'updated_at' => $now
         ]);
+    }
+
+    function userRatings() {
+        return DB::table('ratings')
+            ->select('id')
+            ->where('ratings.user', '=', Auth::id())
+            ->get();
+    }
+
+    function getUsersRatings($request) {
+        return DB::table('ratings')
+            ->join('charging_stations','ratings.charging_station','=', 'charging_stations.id')
+            ->join('locations','charging_stations.location','=', 'locations.id')
+            ->select('ratings.user as user',
+                'ratings.charging_station as charging_station',
+                'ratings.rating as rating'
+            )->where('locations.metropolitan', '=', $request->metropolitan)
+            ->where('locations.ward_number', '=', $request->ward_number)
+            ->get();
+    }
+
+    function getUsersRatingsNoWard($request) {
+        return DB::table('ratings')
+            ->join('charging_stations','ratings.charging_station','=', 'charging_stations.id')
+            ->join('locations','charging_stations.location','=', 'locations.id')
+            ->select('ratings.user as user',
+                'ratings.charging_station as charging_station',
+                'ratings.rating as rating'
+            )->where('locations.metropolitan', '=', $request->metropolitan)
+            ->get();
     }
 }
